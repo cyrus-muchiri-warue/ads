194f4c31711a3073d2e771431c46a42cdfd57123
cyrusmuchiriwarue <cyrus5078@gmail.com>
2020-10-31 15:17:07 +0300

    fist commit

---
+++ app/src/main/java/ke/co/nevon/ads/MainActivity.java
@@ -0,0 +1,21 @@
+package ke.co.nevon.ads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+AdView mAdview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this,"ca-app-pub-3940256099942544~3347511713");
+        AdRequest ar=new AdRequest.Builder().build();
+        mAdview.loadAd(ar);
+
+    }
+}
